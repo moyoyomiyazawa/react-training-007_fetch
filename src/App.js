413f27df87f31b676c9a61cc2362215c7df8b3bd
Yuya Kasai <moyoyomiyazawa@gmail.com>
2021-06-07 18:01:54 +0900

    JSONPlaceholder + axiosの素振り

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const onClickUsers = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onClickUser1 = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users?id=1')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className='App'>
+      <button onClick={onClickUsers}>users</button>
+      <br />
+      <br />
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </div>
+  );
+}
+
+export default App;
